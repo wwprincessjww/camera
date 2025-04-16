f8711ecf6fb03bc4bb50092a0d4c98dd84d597b5
wwprincessjww <mcgoldrickj@olg1.org>
2025-04-16 10:26:23 -0400

    Create custom_usb_camera_block.js

---
+++ custom_usb_camera_block.js
@@ -0,0 +1,18 @@
+Blockly.defineBlocksWithJsonArray([
+  {
+    "type": "usb_camera_take_picture",
+    "message0": "Take a picture with USB camera and save as %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "FILENAME",
+        "text": "image.jpg"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "Take a picture using the USB camera and save it with the specified filename.",
+    "helpUrl": ""
+  }
+]);
